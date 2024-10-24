db43238d5967256a4e919fb82c1eeb7c8ae51e26
Parnashree <dasparnashree16@gmail.com>
2024-10-24 15:20:23 +0530

    Upto page 4 created

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
 var ec = document.querySelector("#elem-cont");
 var fix = document.querySelector("#fixed");
-var hideTimeout = null;  
-ec.addEventListener("mouseenter", function() {
-    if (hideTimeout) clearTimeout(hideTimeout);  
-    fix.style.display = "block";
-    fix.style.pointerEvents = "none";  
+var hideTimeout = null;
+ec.addEventListener("mouseenter", function () {
+  if (hideTimeout) clearTimeout(hideTimeout);
+  fix.style.display = "block";
+  fix.style.pointerEvents = "none";
 });
 
-ec.addEventListener("mouseleave", function() {
-    hideTimeout = setTimeout(function() {
-        fix.style.display = "none";
-    }, 100); 
+ec.addEventListener("mouseleave", function () {
+  hideTimeout = setTimeout(function () {
+    fix.style.display = "none";
+  }, 100);
 });
 
-fix.addEventListener("mouseenter", function() {
-    if (hideTimeout) clearTimeout(hideTimeout); 
-    fix.style.display = "block";
-    fix.style.pointerEvents = "auto"; 
+fix.addEventListener("mouseenter", function () {
+  if (hideTimeout) clearTimeout(hideTimeout);
+  fix.style.display = "block";
+  fix.style.pointerEvents = "auto";
 });
 
-fix.addEventListener("mouseleave", function() {
-    hideTimeout = setTimeout(function() {
-        fix.style.display = "none";
-        fix.style.pointerEvents = "none";  
-    }, 100); 
+fix.addEventListener("mouseleave", function () {
+  hideTimeout = setTimeout(function () {
+    fix.style.display = "none";
+    fix.style.pointerEvents = "none";
+  }, 100);
 });
 
 var e = document.querySelectorAll(".elem");
-e.forEach(function(f) {
-    f.addEventListener("mouseenter", function() {
-        var img = f.getAttribute("img-data");
-        fix.style.backgroundImage = `url(${img})`;
-        fix.style.pointerEvents = "none";  
+e.forEach(function (f) {
+  f.addEventListener("mouseenter", function () {
+    var img = f.getAttribute("img-data");
+    fix.style.backgroundImage = `url(${img})`;
+    fix.style.pointerEvents = "none";
+  });
+});
+
+var cards = ["img1.webp", "img2.webp", "img3.webp"];
+
+var paragraphs = [
+  "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.",
+  "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.",
+  "We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.",
+];
+
+const h1Elements = document.querySelectorAll(".boxleft h1");
+var card = document.querySelector(".boxright");
+var paragraph = document.querySelector(".boxleft p");
+
+h1Elements.forEach((h1, index) => {
+  h1.addEventListener("click", function () {
+    h1Elements.forEach((el) => {
+      el.classList.remove("active");
     });
+    card.style.backgroundImage = `url(${cards[index]})`;
+    paragraph.textContent = paragraphs[index];
+    this.classList.add("active");
+  });
 });
